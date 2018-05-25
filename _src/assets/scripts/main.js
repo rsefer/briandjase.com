ad33e3d30faeac82eb647bc771e4502332eaf0af
Robert Sefer <rsefer@gmail.com>
2018-05-25 14:46:15 -0500

    Roughing in

--- _src/assets/scripts/main.js
+++ _src/assets/scripts/main.js
@@ -1,2 +1,10 @@
-//jQuery(document).ready(function($) {
-//});
+[].forEach.call(document.querySelectorAll('nav a'), function(el) {
+  el.addEventListener('click', function(e) {
+    e.preventDefault();
+    var targetSection = el.getAttribute('href').substring(1);
+    document.querySelector('[data-section="' + targetSection + '"]').scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  })
+})
