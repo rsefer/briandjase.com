ab2623e2751d82c8559cea5003b8e7e0d87e89d4
Robert Sefer <rsefer@gmail.com>
2018-05-27 15:10:23 -0500

    Progress

--- _src/assets/scripts/main.js
+++ _src/assets/scripts/main.js
@@ -6,5 +6,17 @@
       behavior: 'smooth',
       block: 'start'
     });
-  })
-})
+  });
+});
+
+// mapboxgl.accessToken = 'pk.eyJ1Ijoic2VmZXJkZXNpZ24iLCJhIjoiY2pocDZ4ZHlxNDVocDM2bTc5OW05Z3h4YSJ9.Ofd0_UrlAyYYU9kRJVWdRQ';
+//
+// var map = new mapboxgl.Map({
+//   container: 'map',
+//   style: 'mapbox://styles/seferdesign/cjhp7c6fx3uxu2st8p1o7opmv',
+//   zoom: 13,
+//   maxZoom: 14,
+//   center: [-87.661437, 41.887771],
+//   attributionControl: false,
+//   interactive: false
+// });
